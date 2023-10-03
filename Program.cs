27ba6dd3de21df64d5a46d8191485e2f160bd7f0
Carlos Barros <carlos_dbs@hotmail.com>
2023-10-03 14:31:47 -0300

    Initial commit with Razor Page configuration

---
+++ Program.cs
@@ -0,0 +1,14 @@
+var builder = WebApplication.CreateBuilder(args);
+
+//Adds support to the service RazorPage
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+
+app.UseHttpsRedirection();//Enables the use https
+app.UseStaticFiles();//Enables the use wwwroot with static files
+
+app.UseRouting();//Adds middleware to matching routes 
+app.MapRazorPages();//Defines endpoints of the application
+
+app.Run();

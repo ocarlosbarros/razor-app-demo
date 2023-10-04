dac55bc4de1a57808e937a005320988de5ac63d8
Carlos Barros <carlos_dbs@hotmail.com>
2023-10-04 12:21:10 -0300

    refactor(Index):change code base to categories page

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -4,21 +4,9 @@ namespace RazorAppDemo.Pages
 {
     public class Index : PageModel
     {
-        public List<Category> Categories { get; set; } = new();
-        public async Task OnGet()
+        
+        public void OnGet()
         {
-            await Task.Delay(5000);
-            for (int index = 0; index <= 100; index += 1)
-            {
-                Categories.Add(
-                    new Category(index, Title:$"Categoria {index}", Price: index * 18.95M));
-            }
         }
     }
 }
-
-public record Category (
-    int Id,
-    string Title,
-    decimal Price
-);

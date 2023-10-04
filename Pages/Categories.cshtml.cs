9952932f5c9b7b06dba1a08a829b9af8f7a47f9b
Carlos Barros <carlos_dbs@hotmail.com>
2023-10-04 14:02:08 -0300

    feat(Categories):adds pagination

--- Pages/Categories.cshtml.cs
+++ Pages/Categories.cshtml.cs
@@ -5,19 +5,32 @@ namespace RazorAppDemo.Pages
     public class Categories : PageModel
     {
         public List<Category> CategoryList { get; set; } = new();
-        public void OnGet()
+
+        public void OnGet(
+            int skip = 0, 
+            int take = 25)
         {
-            for (int index = 0; index <= 100; index += 1)
+            var temp = new List<Category>();
+
+            for (int index = 0; index <= 1787; index += 1)
             {
-                CategoryList.Add(
+                temp.Add(
                     new Category(index, Title:$"Categoria {index}", Price: index * 18.95M));
             }
+
+        CategoryList = temp
+            .Skip(skip)
+            .Take(take)
+            .ToList();
         }
+       
     }
+
+    public record Category (
+        int Id,
+        string Title,
+        decimal Price
+    );
 }
 
-public record Category (
-    int Id,
-    string Title,
-    decimal Price
-);
+
